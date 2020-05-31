ffc248dedaf6cf259fea6875d103ca6a37ec7e6d
Facundo Pereyra <fpereyra@MacBook-Pro-de-Facundo.local>
2020-05-30 23:15:09 -0300

    public repo

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# aws_update_credentials_from_sso
-Update Credentials File from SSO in AWS
+# Get AWS SSO Credentials 
+
+Python Script for updating the local credentials file in your computer using the SSO Login. This script creates a [account-role] entry in your credentials file. 
+Each time you run the script it will update each file but not overwrite it. 
+
+## Pre requisites
+
+* Latest Version of the AWS Cli
+* Boto 1.13.19 or grater
+* Python lib SafeConfigParser
+* Python lib pathlib
+
+## Configuration
+
+Before running the script its important to update line 10 with the sso start url as shown below:
+
+    sso_start_url = 'https://cloud(your_company).awsapps.com/start#/'
+
+If you would like to update the default region you can do thtat by updating the line 8 as shown below:
+
+    AWS_DEFAULT_REGION = 'us-east-1'
+
+## Usage
+
+1. Run python3  get-creds.py
+2. It will take you to the SSO login page. 
+3. Enter your credentials.
+4. When asked to back to the terminal and press enter. 
+5. And thats it! :) 
+
+## References: 
+
+boto3 
+
+    https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso-oidc.html
+    https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/sso.html
+AWS
+
+    https://docs.aws.amazon.com/singlesignon/latest/OIDCAPIReference/API_CreateToken.html
+    https://docs.aws.amazon.com/singlesignon/latest/OIDCAPIReference/API_RegisterClient.html
+    https://docs.aws.amazon.com/singlesignon/latest/PortalAPIReference/API_GetRoleCredentials.html
+
+## Credits
+
+    Facundo Pereyra / f.pereyrap@gmail.com
+    Matias Carosso / carossomatias@gmail.com
---
+++ get-creds.py
@@ -0,0 +1,129 @@
+#!/usr/bin/env python3
+
+import boto3
+import json, webbrowser, socket
+from pathlib import Path
+from configparser import SafeConfigParser
+
+AWS_DEFAULT_REGION = 'us-east-1'
+AWS_CREDENTIAL_PATH = f'{Path.home()}/.aws/credentials'
+sso_start_url = ''
+
+client = boto3.client('sso-oidc')
+sso_client = boto3.client('sso')
+
+def read_config(path):
+    config = SafeConfigParser()
+    config.read(path)
+    return config
+
+def write_config(path, config):
+    with open(path, "w") as destination:
+        config.write(destination)
+
+def device_registration(client_name, client_type):
+    try:
+        response_client_registration = client.register_client(
+            clientName= client_name,
+            clientType= client_type,
+        )
+        return response_client_registration['clientId'], response_client_registration['clientSecret']
+    except Exception as e:
+        return e
+
+def get_auth_device(id, secret, start_url):
+    try:
+        response_device_authorization = client.start_device_authorization(
+            clientId=id,
+            clientSecret=secret,
+            startUrl=start_url
+        )
+        return response_device_authorization['verificationUriComplete'], response_device_authorization['deviceCode'], response_device_authorization['userCode']
+    except Exception as e:
+        return e
+
+
+def get_token(id, secret, device_code, user_code):
+    try:
+        response_token_creation = client.create_token(
+            clientId= id,
+            clientSecret= secret,
+            grantType='urn:ietf:params:oauth:grant-type:device_code', #review
+            deviceCode= device_code,
+            code= user_code
+        )
+        return response_token_creation['accessToken']
+    except Exception as e:
+        return e
+
+def get_list_accounts(token):
+    try:
+        response_list_accounts = sso_client.list_accounts(
+            # nextToken='string',
+            maxResults=123,
+            accessToken=token
+        )
+        return response_list_accounts['accountList']
+    except Exception as e:
+        return e
+
+def get_roles_account(token, accountid):
+    try:
+        response_account_roles = sso_client.list_account_roles(
+            # nextToken='string',
+            maxResults=123,
+            accessToken=token,
+            accountId=accountid
+        )
+        return response_account_roles['roleList']
+    except Exception as e:
+        return e
+
+def get_roles_credentials(rolename, accountid, token):
+    try:
+        response_role_credentials = sso_client.get_role_credentials(
+            roleName=rolename,
+            accountId=accountid,
+            accessToken=token
+        )
+        return response_role_credentials['roleCredentials']
+    except Exception as e:
+        return e
+
+def update_aws_credentials(profile_name, profile, credentials):
+    region = AWS_DEFAULT_REGION
+    config = read_config(AWS_CREDENTIAL_PATH)
+    full_name = profile_name + "-" + profile
+    if config.has_section(full_name):
+        config.remove_section(full_name)
+    config.add_section(full_name)
+    config.set(full_name, "region", region)
+    config.set(full_name, "aws_access_key_id", credentials["accessKeyId"])
+    config.set(full_name, "aws_secret_access_key ", credentials["secretAccessKey"])
+    config.set(full_name, "aws_session_token", credentials["sessionToken"])
+    write_config(AWS_CREDENTIAL_PATH, config)
+
+clientId, clientSecrets = device_registration(socket.gethostname(), 'public')
+url, deviceCode, userCode = get_auth_device(clientId, clientSecrets, sso_start_url)
+
+try:
+    webbrowser.open(url)
+except:
+    print("Please manual login: %s \n" % (url))
+
+input("After login, press Enter to continue...")
+
+token = get_token(clientId, clientSecrets, deviceCode, userCode)
+
+accounts_list = get_list_accounts(token)
+
+for account in accounts_list:
+    account_id = account['accountId']
+    account_name = account['accountName']
+    role_name_data = get_roles_account(token, account_id)
+    for roleName in role_name_data:
+        role_name = roleName ['roleName']
+        account = roleName['accountId']
+        temp_credentials = get_roles_credentials(role_name, account, token)
+        print (account_name, 'created')
+        update_aws_credentials(account_name, role_name, temp_credentials)
